62724e3ae6059e07bdd7ed4bd7c5f967c7f3a084
Maximoo <maximtz99@gmail.com>
2022-08-17 15:13:35 -0500

    added local-storage feature, that persists through refresh

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,35 +2,60 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-timeTrack();
+$(document).ready(function () {
 
-function timeTrack() {
-    //get current number of hours.
-    var timePres = moment().hour();
-
-    // loop over timeblocks
-    $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-        // Check the time and add the classes to change bg color accordingly
-        if (blockTime < timePres) {
-            $(this).removeClass("future");
-            $(this).removeClass("present");
-            $(this).addClass("past");
-        }
-        else if (blockTime === timePres) {
-            $(this).removeClass("past");
-            $(this).removeClass("future");
-            $(this).addClass("present");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
+    // Listener for save button click
+    $(".saveBtn").on("click", function () {
+        // On click, gathers values from "description" element
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        // saves time and text into local storage
+        localStorage.setItem(time, text)
     })
-};
 
-setInterval(function () {
-    timeTrack();
-}, (1000 * 60) * 10);
+    function timeTrack() {
+        //get current number of hours.
+        var timePres = moment().hour();
+
+        // loop over timeblocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // Check the time and add the classes to change bg color accordingly
+            if (blockTime < timePres) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timePres) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    //getItem from local storage if available
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+    // runs the timeTrack function every 10 minutes
+    setInterval(function () {
+        timeTrack()
+    }, (1000 * 60) * 10);
+
+    timeTrack()
+});
