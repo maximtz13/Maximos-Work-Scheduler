d8d25203a99d749caad573b1655ff69ef1dfd841
Maximoo <maximtz99@gmail.com>
2022-08-17 14:42:50 -0500

    added color-code feature

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,3 +2,33 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+timeTrack();
+
+function timeTrack() {
+    //get current number of hours.
+    var timePres = moment().hour();
+
+    // loop over timeblocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // Check the time and add the classes to change bg color accordingly
+        if (blockTime < timePres) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timePres) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+};
+
+
